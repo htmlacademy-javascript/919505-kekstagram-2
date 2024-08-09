0f184294bfd14c75dda26c8c948e7f6131972581
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-08-09 15:42:17 +0300

    Исправляет замечания линтера

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@ const convertTimeToMinutes = (time) => {
   const normalisedMinute = +minute;
 
   return normalisedHour * 60 + normalisedMinute;
-}
+};
 
 const isMeetingValid = (dayStart, dayEnd, meetingStart, meetingDuration) => {
   const start = convertTimeToMinutes(dayStart);
@@ -14,10 +14,10 @@ const isMeetingValid = (dayStart, dayEnd, meetingStart, meetingDuration) => {
 
   // eslint-disable-next-line no-console
   console.log(start <= meeting && meeting <= end);
-}
+};
 
 isMeetingValid('08:00', '17:30', '14:00', 90); // true
-isMeetingValid('8:0', '10:0', '8:0', 120);     // true
+isMeetingValid('8:0', '10:0', '8:0', 120); // true
 isMeetingValid('08:00', '14:30', '14:00', 90); // false
-isMeetingValid('14:00', '17:30', '08:0', 90);  // false
+isMeetingValid('14:00', '17:30', '08:0', 90); // false
 isMeetingValid('8:00', '17:30', '08:00', 900); // false
