794965586dbbe90f4dca79828ffd5894b3dc6165
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-08-17 00:24:28 +0300

    Добавляет закрытие карточки через escape

--- js/components/photo-card.js
+++ js/components/photo-card.js
@@ -1,3 +1,5 @@
+import {KeyCode} from '../const.js';
+
 const photoCard = document.querySelector('.big-picture');
 const closeButton = photoCard.querySelector('.big-picture__cancel');
 
@@ -10,13 +12,27 @@ const updateCardContent = (cardData) => {
 };
 
 const closePhotoCardHandler = () => {
-  photoCard.classList.add('hidden');
-  closeButton.removeEventListener('click', closePhotoCardHandler);
+  closePhotoCard();
 };
+
+const keydownHandler = (evt) => {
+  if (evt.keyCode === KeyCode.ESC) {
+    closePhotoCard();
+  }
+};
+
+function closePhotoCard () {
+  document.removeEventListener('keydown', keydownHandler);
+  closeButton.removeEventListener('click', closePhotoCardHandler);
+
+  photoCard.classList.add('hidden');
+}
 
 export const openPhotoCard = (cardData) => {
   updateCardContent(cardData);
 
-  closeButton.addEventListener('click', closePhotoCardHandler);
   photoCard.classList.remove('hidden');
+
+  closeButton.addEventListener('click', closePhotoCardHandler);
+  document.addEventListener('keydown', keydownHandler);
 };
