6acd0d4d25e3ca344501dfe07fbd1203177c947d
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-08-16 23:17:24 +0300

    Оживляет превьюшки WIP

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 import {generatePhotosData} from './data/photos.js';
 import {renderThumbnailList} from './components/thumbnail-list.js';
+import {initThumbnail} from './components/thumbnail-item.js';
+import {openPhotoCard} from './components/photo-card.js';
 
+// Собираем данные фотографий в объект
 const photosData = generatePhotosData();
 
+// Рендерим превьюшки
 renderThumbnailList(photosData);
+
+// Оживляем отрендеренные превьюшки
+const thumbnails = document.querySelectorAll('.picture');
+thumbnails.forEach((thumbnail) => initThumbnail(thumbnail, openPhotoCard));
