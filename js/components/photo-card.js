78fd1058eaf37e6f16584f84fffae5b60c115844
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-08-16 23:18:41 +0300

    Исправляет замечания линтера

--- js/components/photo-card.js
+++ js/components/photo-card.js
@@ -4,21 +4,19 @@ const closeButton = photoCard.querySelector('.big-picture__cancel');
 const closePhotoCardHandler = () => {
   photoCard.classList.add('hidden');
   closeButton.removeEventListener('click', closePhotoCardHandler);
-}
+};
 
 const updateCardData = (cardData) => {
   const {url, likes, totalCommentsCount} = cardData;
   photoCard.querySelector('.big-picture__img img').src = url;
   photoCard.querySelector('.likes-count').textContent = likes;
   photoCard.querySelector('.social__comment-total-count').textContent = totalCommentsCount;
-}
+};
 
 export const openPhotoCard = (thumbnail) => {
   const url = thumbnail.querySelector('.picture__img').src;
   const likes = thumbnail.querySelector('.picture__likes').textContent;
   const totalCommentsCount = thumbnail.querySelector('.picture__comments').textContent;
-
-  console.log(thumbnail);
 
   updateCardData({url, likes, totalCommentsCount});
   photoCard.classList.remove('hidden');
