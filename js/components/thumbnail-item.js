696db14e728280544efd9676657bbc7a2af54bfa
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-08-17 19:26:50 +0300

    Добавляет делегирование

--- js/components/thumbnail-item.js
+++ js/components/thumbnail-item.js
@@ -2,25 +2,19 @@ const thumbnailTemplate = document.querySelector('#picture').content;
 
 // Создает превьюшку для последующего рендера
 export const createThumbnail = (data) => {
-  const {url, description, likes, comments} = data;
+  const {id, url, description, likes, comments} = data;
 
   const newThumbnail = thumbnailTemplate.cloneNode(true);
+  const hrefElement = newThumbnail.querySelector('a');
   const imgElement = newThumbnail.querySelector('.picture__img');
   const likesElement = newThumbnail.querySelector('.picture__likes');
   const commentsElement = newThumbnail.querySelector('.picture__comments');
 
+  hrefElement.dataset.userId = id;
   imgElement.src = url;
   imgElement.alt = description;
   likesElement.textContent = likes;
   commentsElement.textContent = comments.length;
 
   return newThumbnail;
-};
-
-// Оживляет отрендеренную превьюшку
-export const initThumbnail = (thumbnail, data, callback) => {
-  thumbnail.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    callback(data);
-  });
 };
--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 import {generatePhotosData} from './data/photos.js';
-import {renderThumbnailList} from './components/thumbnail-list.js';
-import {initThumbnail} from './components/thumbnail-item.js';
+import {renderThumbnails, initThumbnailList} from './components/thumbnail-list.js';
 import {openPhotoCard} from './components/photo-card.js';
 
 // Собираем данные фотографий в объект
 const photosData = generatePhotosData();
 
 // Рендерим превьюшки
-renderThumbnailList(photosData);
+renderThumbnails(photosData);
 
-// Оживляем отрендеренные превьюшки
-// В данном случае это будет вызов коллбэка openPhotoCard при клике на превьюшку
-const thumbnails = document.querySelectorAll('.picture');
-thumbnails.forEach((thumbnail, i) => initThumbnail(thumbnail, photosData[i], openPhotoCard));
+// Оживляем превюшки через делегирование
+// Передаем коллбэк openPhotoCard, который будет вызываться при клике на список превьюшек
+initThumbnailList(photosData, openPhotoCard);
