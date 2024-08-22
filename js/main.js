324702c0016760130ffa79d4a631885a13812f5e
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-08-22 11:39:51 +0300

    Добавляет открытие формы загрузки фото

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {generatePhotosData} from './data/photos.js';
 import {renderPreviews, initPreviewList} from './components/preview-list.js';
 import {openPhotoCard} from './components/photo-card.js';
+import {initUploadForm} from './components/form-photo-upload';
 
 // Собираем данные фотографий в объект
 const photosData = generatePhotosData();
@@ -11,3 +12,6 @@ renderPreviews(photosData);
 // Оживляем превьюшки через делегирование
 // Передаем коллбэк openPhotoCard, который будет вызываться при клике на список превьюшек
 initPreviewList(photosData, openPhotoCard);
+
+// Оживляем форму загрузки фото
+initUploadForm();
